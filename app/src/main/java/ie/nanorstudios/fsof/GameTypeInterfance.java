983f205d9e651e8c8ed4c294a0efda4e96754e33
Ronan <ronandoyle91@gmail.com>
2016-04-28 00:56:51 +0100

    After several years of experience and skill growth - I've scrapped most of the awful awful code I wrote before and have started fresh. There may be one or two outlier files that will get deleted along the way, but this is a good starting point.
    - Added Butterknife to make view interaction easier.
    
    Much much more to come!

---
+++ app/src/main/java/ie/nanorstudios/fsof/GameTypeInterfance.java
@@ -0,0 +1,13 @@
+package ie.nanorstudios.fsof;
+
+/**
+ * Created by Ronan on 28/04/2016.
+ */
+public interface GameTypeInterfance {
+
+    int ALL = 0;
+    int ACTION = 1;
+    int COMEDY = 2;
+    int ROMANCE = 3;
+    int SCIFI = 4;
+}
---
+++ app/src/main/java/ie/nanorstudios/fsof/activities/RoundActivity.java
@@ -0,0 +1,43 @@
+package ie.nanorstudios.fsof.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ie.nanorstudios.fsof.GameTypeInterfance;
+
+/**
+ * Created by Ronan on 28/04/2016.
+ */
+public class RoundActivity extends Activity {
+
+    private int mGameType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: 28/04/2016 Create activity ui.
+
+        if (savedInstanceState != null) {
+            mGameType = savedInstanceState.getInt("GAME_TYPE");
+            displayGameTypeSlashScreen();
+        }
+    }
+
+    private void displayGameTypeSlashScreen() {
+        switch (mGameType) {
+            // TODO: 28/04/2016 Show view with relevant picture, then load show and populate a round.
+            case GameTypeInterfance.ALL:
+                break;
+            case GameTypeInterfance.ACTION:
+                break;
+            case GameTypeInterfance.COMEDY:
+                break;
+            case GameTypeInterfance.ROMANCE:
+                break;
+            case GameTypeInterfance.SCIFI:
+                break;
+            default:
+                break;
+        }
+    }
+}
