09ca4f945f47a35777fcb04de382ba59e054fdc3
Ronan Doyle <ronan@donedeal.ie>
2016-09-07 23:05:38 +0100

    Updated to latest build tools.

--- app/build/generated/source/r/debug/ie/nanorstudios/fsof/R.java
+++ app/build/generated/source/r/debug/ie/nanorstudios/fsof/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int all_genres=0x7f020000;
@@ -25,18 +25,19 @@ public final class R {
         public static final int stars4=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int btn_action=0x7f090001;
-        public static final int btn_all=0x7f090000;
-        public static final int btn_comedy=0x7f090002;
-        public static final int btn_romance=0x7f090003;
-        public static final int btn_scifi=0x7f090004;
-        public static final int frame_layout=0x7f090006;
-        public static final int iv_stars_footer=0x7f090005;
-        public static final int tv_and=0x7f090009;
-        public static final int tv_first_name=0x7f090008;
-        public static final int tv_round_title=0x7f090007;
-        public static final int tv_second_name=0x7f09000a;
+        public static final int action_settings=0x7f08000c;
+        public static final int btn_action=0x7f080001;
+        public static final int btn_all=0x7f080000;
+        public static final int btn_comedy=0x7f080002;
+        public static final int btn_romance=0x7f080003;
+        public static final int btn_scifi=0x7f080004;
+        public static final int frame_layout=0x7f080006;
+        public static final int iv_stars_footer=0x7f080005;
+        public static final int tv_and=0x7f080009;
+        public static final int tv_first_name=0x7f080008;
+        public static final int tv_round_title=0x7f080007;
+        public static final int tv_second_name=0x7f08000a;
+        public static final int tv_timer=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,15 +45,14 @@ public final class R {
         public static final int fragment_round=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-    }
-    public static final class raw {
-        public static final int countdown=0x7f040000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int ellipsis=0x7f050002;
+        public static final int hello_world=0x7f050003;
+        public static final int round_title=0x7f050004;
     }
     public static final class style {
         /** 
@@ -61,9 +61,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
